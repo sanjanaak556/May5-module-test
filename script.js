fec5ffef68309b0798fb9848513b5f9115ec7028
sanjanaak556 <117841426+sanjanaak556@users.noreply.github.com>
2023-05-06 11:06:25 +0530

    final

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+const sortButtons = document.querySelectorAll(".sort");
 fetch('MOCK_DATA.json')
   .then(response => response.json())
   .then(data =>{
@@ -27,3 +27,34 @@ fetch('MOCK_DATA.json')
       result.innerHTML += htmlData
     }
   }
+
+sortButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const sortBy = button.getAttribute("data-sort");
+    switch (sortBy) {
+      case "name-asc":
+        data.sort((a, b) => a.first_name.localeCompare(b.first_name));
+        break;
+      case "name-desc":
+        data.sort((a, b) => b.first_name.localeCompare(a.first_name));
+        break;
+      case "marks":
+        data.sort((a, b) => a.marks - b.marks);
+        break;
+      case "passing":
+        const passingData = studentsData.filter((student) => student.passing);
+        data(passingData);
+        return;
+      case "data":
+        studentsData.sort((a, b) => a.class.localeCompare(b.class));
+        break;
+      case "gender":
+        const femaleData = studentsData.filter((student) => student.gender === "female");
+        const maleData = studentsData.filter((student) => student.gender === "male");
+        tablebody.innerHTML = "";
+        
+        return;
+    }
+    displayData(data);
+  });
+});
